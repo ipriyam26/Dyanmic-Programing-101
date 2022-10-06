bad55fde24e5ca90758125031ab4b9cc45d6a38a
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-06 22:14:25 +0530

    Scripted auto-commit on change (2022-10-06 22:14:25) by gitwatch.sh

--- knapsack01-top-down.py
+++ knapsack01-top-down.py
@@ -1,5 +1,6 @@
 #User function Template for python3
 import itertools
+from pprint import pprint
 class Solution:
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val, N):
@@ -9,7 +10,7 @@ class Solution:
             dp[i][0]=0
         for i in range(W+1):
             dp[0][i]=0
-
+        pprint(dp)
         for i, j in itertools.product(range(1,N+1), range(1,W+1)):
             dp[i][j] = dp[i - 1][j] if (wt[i - 1] > W) else max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]])
         return dp[N][W]
