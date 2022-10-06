2d0145416f34f56b0203d8a850c9a228d4b97b36
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-06 21:34:22 +0530

    first commit

---
+++ knapsack01-base.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    #Function to return max value that can be put in knapsack of capacity W.
+    def knapSack(self,W, wt, val, N):
+        
+        
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        W = int(input())
+        val = list(map(int,input().strip().split()))
+        wt = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.knapSack(W,wt,val,n))
+# } Driver Code Ends
