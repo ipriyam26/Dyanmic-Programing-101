2a0e14d8275f939da9bc6bc4d389fd6c692345c3
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-08 21:27:06 +0530

    Scripted auto-commit on change (2022-10-08 21:27:06) by gitwatch.sh

--- knapsack-pattern/equal-sum-partition/base.py
+++ knapsack-pattern/equal-sum-partition/base.py
@@ -1,5 +1,8 @@
 #User function Template for python3
 
+from typing import List
+
+
 class Solution:
     #Function to return max value that can be put in knapsack of capacity W.
     def subset_sum(self,sum_needed,n,wt):
@@ -12,6 +15,12 @@ class Solution:
         else:
             return  self.subset_sum(sum_needed,n-1,wt) 
         
+    def canPartition(self,arr:List[int]) -> bool:
+        sum_arr = sum(arr)/2
+        if int(sum_arr)!=sum_arr or sum_arr%2==1 or sum_arr in arr:
+            return False
+        return self.subset_sum(sum_arr,len(arr),arr) 
+
 
 if __name__ == '__main__':
     test_cases = int(input())
