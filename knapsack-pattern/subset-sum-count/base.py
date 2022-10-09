a662eb889e20772f88ff2d74f5e1b89a08a4baea
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-09 23:17:42 +0530

    subset sum count base

--- knapsack-pattern/subset-sum-count/base.py
+++ knapsack-pattern/subset-sum-count/base.py
@@ -7,21 +7,23 @@ class Solution:
     #Function to return max value that can be put in knapsack of capacity W.
     def subset_sum(self,sum_needed,n,wt):
         if sum_needed ==0:
-            return True
-        if n==0:
-            return False
-        if sum_needed>wt[n-1]:
-            return self.subset_sum(sum_needed,n-1,wt) or self.subset_sum(sum_needed-wt[n-1],n-1,wt)
-        else:
-            return  self.subset_sum(sum_needed,n-1,wt) 
+            return 1 
+        if n == 0:
+            return 0
+        if(sum_needed<wt[n-1]):
+            return self.subset_sum(sum_needed=sum_needed,n=n-1,wt=wt)
+        
+        return self.subset_sum(sum_needed,n-1,wt) + self.subset_sum(sum_needed-wt[n-1],n-1,wt)
+             
         
         
 
 if __name__ == '__main__':
-    test_cases = int(input())
-    for _ in range(test_cases):
-        sum_needed= int(input())
-        wt = list(map(int,input().strip().split()))
+    # test_cases = int(input())
+    # for _ in range(test_cases):
+        # sum_needed= int(input())
+        # wt = list(map(int,input().strip().split()))
         ob=Solution()
-        print(ob.subset_sum(sum_needed=sum_needed,n=len(wt),wt=wt))
+        wt = [2,3,5,6,8,10]
+        print(ob.subset_sum(sum_needed=10,n=len(wt),wt=wt))
 # } Driver Code Ends
