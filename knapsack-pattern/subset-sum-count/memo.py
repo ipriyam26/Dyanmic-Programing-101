f7f18d777d27a83756bd41f66f49b948ba5be923
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-10 14:47:51 +0530

    Subset sum count done

--- knapsack-pattern/subset-sum-count/memo.py
+++ knapsack-pattern/subset-sum-count/memo.py
@@ -6,23 +6,29 @@ from typing import List
 class Solution:
     #Function to return max value that can be put in knapsack of capacity W.
     def launch(self,sum_needed,n,wt):
-        self.dp= [[None for _ in range(n+1)] for _ in range(sum_needed+1)]
+        self.dp= [[None for _ in range(sum_needed+1)] for _ in range(n+1)]
         return self.subset_sum(sum_needed=sum_needed,n=n,wt=wt)
     
     def subset_sum(self,sum_needed,n,wt):
-        if sum_needed ==0:return 1
-        if n==0:return 0
-        if self.dp[sum_needed][n]!=None: return self.dp[sum_needed][n]
-        if sum_needed<wt[n-1]:self.dp[sum_needed][n]=  self.subset_sum(sum_needed,n-1,wt)
-        else:self.dp[sum_needed][n] =self.subset_sum(sum_needed,n-1,wt) + self.subset_sum(sum_needed-wt[n-1],n-1,wt)
-        return self.dp[sum_needed][n]
+        if sum_needed ==0:
+            return 1
+        if n==0:
+            return 0
+        if self.dp[n][sum_needed]!=None:
+            return self.dp[n][sum_needed]
+        if wt[n-1]>sum_needed:
+            self.dp[n][sum_needed]=  self.subset_sum(sum_needed,n-1,wt)
+        else:
+            self.dp[n][sum_needed] =self.subset_sum(sum_needed,n-1,wt) + self.subset_sum(sum_needed-wt[n-1],n-1,wt)
+        return self.dp[n][sum_needed]
         
 
 if __name__ == '__main__':
     test_cases = int(input())
     for _ in range(test_cases):
-        sum_needed= int(input())
-        wt = list(map(int,input().strip().split()))
-        ob=Solution()
-        print(ob.launch(sum_needed=sum_needed,n=len(wt),wt=wt))
+        # sum_needed= int(input())
+        # wt = list(map(int,input().strip().split()))
+        # ob=Solution()
+        wt=[3,5,6,7,1,2]
+        print(Solution().launch(sum_needed=7,n=len(wt),wt=wt))
 # } Driver Code Ends
--- knapsack-pattern/subset-sum-count/top-down.py
+++ knapsack-pattern/subset-sum-count/top-down.py
@@ -11,12 +11,12 @@ class Solution:
         dp= [[None for _ in range(sum_needed+1)] for _ in range(n+1)]
 
         for i in range(sum_needed+1):
-            dp[0][i]=False
+            dp[0][i]=0
         for i in range(n+1):
-            dp[i][0]=True
+            dp[i][0]=1
 
         for i, j in itertools.product(range(1,n+1), range(1,sum_needed+1)):
-            dp[i][j] = dp[i-1][j] if j > wt[i-1] else dp[i-1][j] or dp[i-1][j-wt[i-1]]
+            dp[i][j] = dp[i-1][j] if j > wt[i-1] else dp[i-1][j] + dp[i-1][j-wt[i-1]]
             
         return dp[i][j]
 
