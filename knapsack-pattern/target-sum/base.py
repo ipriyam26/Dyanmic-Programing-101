e0cfdb2850f469e5edae48f8b1b6d558cc8ccb8f
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-11 19:31:38 +0530

    Scripted auto-commit on change (2022-10-11 19:31:38) by gitwatch.sh

---
+++ knapsack-pattern/target-sum/base.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    
+    def runner(self,arr: List[int],sum:int,n:int) -> int:
+        
+        if (n==0):
+            return 1 if sum==self.target else 0
+
+        if (n,sum) in self.hash:
+            return self.hash[(n,sum)]
+        
+        self.hash[(n,sum)] = self.runner(arr,sum+arr[n-1],n-1)+self.runner(arr,sum-arr[n-1],n-1)
+        return self.hash[(n,sum)]
+        
+        
+        
+    def targetSum(self,arr: List[int],target:int,n:int):
+        self.target = target
+        self.sum_arr = sum(arr)
+        self.hash = {}
+        self.runner(arr,0,len(arr))
+        
+if __name__ == "__main__":
+    arr = [1,1,1,1,1]
+    s = 3
+    print(Solution().targetSum(arr,s,len(arr)))
+    
