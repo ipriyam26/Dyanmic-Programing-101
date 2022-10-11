43128a2702a89ab0ec6ecff36422011e820500e6
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-11 21:15:26 +0530

    Scripted auto-commit on change (2022-10-11 21:15:26) by gitwatch.sh

--- Unbounded-knapsack/minium-change/memo.py
+++ Unbounded-knapsack/minium-change/memo.py
@@ -7,23 +7,28 @@ class Solution:
     #Function to return max value that can be put in knapsack of capacity W.
     def launch(self,sum_needed,n,wt):
         self.dp= [[None for _ in range(sum_needed+1)] for _ in range(n+1)]
-        return self.subset_sum(sum_needed=sum_needed,n=n,wt=wt)
+        return self.subset_sum(sum_needed=sum_needed,n=n,wt=wt,taken=0)
     
-    def subset_sum(self,sum_needed,n,wt):
+    def subset_sum(self,sum_needed,n,wt,taken):
         if sum_needed ==0:
-            return 1
+            return taken
         if n==0:
-            return 0
+            return 99999
         if self.dp[n][sum_needed]!=None:
             return self.dp[n][sum_needed]
         if wt[n-1]>sum_needed:
-            self.dp[n][sum_needed]=  self.subset_sum(sum_needed,n-1,wt)
+            self.dp[n][sum_needed]=  self.subset_sum(sum_needed,n-1,wt,taken)
         else:
-            self.dp[n][sum_needed] =self.subset_sum(sum_needed,n-1,wt) + self.subset_sum(sum_needed-wt[n-1],n,wt)
+            self.dp[n][sum_needed] =min(self.subset_sum(sum_needed,n-1,wt.taken),self.subset_sum(sum_needed-wt[n-1],n,wt,taken+1))
         return self.dp[n][sum_needed]
 
-    def count(self, coins, N, sum):
-        return self.launch(sum,N,coins)
+    def coinChange(self, coins, sum):
+        if sum==0:
+            return 0
+
+        n = self.subset_sum(sum,len(coins),coins,0)
+        return -1 if n==99999 else n
+        
         
 
 if __name__ == '__main__':
