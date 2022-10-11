1fa897ee2966e89f593591e7f9e506f6e3259655
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-11 19:35:33 +0530

    Scripted auto-commit on change (2022-10-11 19:35:33) by gitwatch.sh

--- Unbounded-knapsack/unbounded-knapsack/memo.py
+++ Unbounded-knapsack/unbounded-knapsack/memo.py
@@ -2,13 +2,24 @@
 
 class Solution:
     def knapSack(self,W, wt, val, N):
+        self.dp =[[-1 for _ in range(W+1)] for _ in range(N+1)]
+        # print(self.dp)
+        return self.runner(W,wt,val,N)
+        
+    
+    def runner(self,W, wt, val, N):
         if N==0 or W==0:
             return 0
+        if self.dp[N][W]!=-1:
+            return self.dp[N][W]
+        
         if(wt[N-1]>W)  :
-            return self.knapSack(W=W,wt=wt,val=val,N=N-1)
-        return max(self.knapSack(W=W,wt=wt,val=val,N=N-1),
-                   val[N-1]+self.knapSack(W=W-wt[N-1],wt=wt,val=val,N=N)
+            self.dp[N][W]= self.runner(W=W,wt=wt,val=val,N=N-1)
+        else:
+            self.dp[N][W]=max(self.runner(W=W,wt=wt,val=val,N=N-1),
+                   val[N-1]+self.runner(W=W-wt[N-1],wt=wt,val=val,N=N)
                    )
+        return self.dp[N][W]
 
         # code here
 
