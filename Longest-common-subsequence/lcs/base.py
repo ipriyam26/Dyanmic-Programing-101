95d9b8a42bbb63774866ac143176f825a1c28ebd
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-14 18:34:54 +0530

    LCS - base

--- Longest-common-subsequence/lcs/base.py
+++ Longest-common-subsequence/lcs/base.py
@@ -10,18 +10,15 @@ class Solution:
     def lcs(self, t1:str,t2:str, n:int, m:int)->int:
         if n==0 or m==0:
             return 0
-        if self.dp[n][m] !=-1:
-            return self.dp[n][m]
         
         if t1[n-1]==t2[m-1]:
-            self.dp[n][m]= 1+ self.lcs(t1=t1,t2=t2,n=n-1,m=m-1)
+            return 1+ self.lcs(t1=t1,t2=t2,n=n-1,m=m-1)
         else:
-            self.dp[n][m] =max(self.lcs(t1,t2,n-1,m),self.lcs(t1,t2,n,m-1))
-        return self.dp[n][m]
+            return max(self.lcs(t1,t2,n-1,m),self.lcs(t1,t2,n,m-1))
+        # return self.dp[n][m]
         
     
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        self.dp = [[ -1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
         return self.lcs(t1=text1,t2=text2,n=len(text1),m=len(text2))
 
 if __name__ == "__main__":
