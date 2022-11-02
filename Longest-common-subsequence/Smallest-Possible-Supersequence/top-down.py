87f4fca8518621d2e5a521f9a814b157acc59335
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-02 18:38:22 +0530

    Smallest Possible Supersequence

---
+++ Longest-common-subsequence/Smallest-Possible-Supersequence/top-down.py
@@ -0,0 +1,42 @@
+import itertools
+#User function Template for python3
+class Solution:
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m=len(text2)
+        n = len(text1)
+        self.dp = [[ -1 for _ in range(m+1)] for _ in range(n+1)]
+        for i in range (n+1):
+            self.dp[i][0]=0
+
+        for j in range(m+1):
+            self.dp[0][j] =0
+
+        i,j=0,0
+        for i, j in itertools.product(range(1,n+1), range(1,m+1)):
+            if text1[i-1] == text2[j-1]:
+                self.dp[i][j] = self.dp[i - 1][j - 1] +1 
+            else:
+                self.dp[i][j]=max(self.dp[i - 1][j], self.dp[i][j - 1])
+
+        return self.dp[n][m]
+    
+    #Function to find length of shortest common supersequence of two strings.
+    def shortestCommonSupersequence(self, X, Y, m, n):
+        
+        return m+n-self.longestCommonSubsequence(X,Y)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__': 
+    t=int(input())
+    for tcs in range(t):
+        X,Y=input().split()
+        
+        print(Solution().shortestCommonSupersequence(X,Y,len(X),len(Y)))
+        
+# } Driver Code Ends
