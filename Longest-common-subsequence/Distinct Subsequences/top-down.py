009a0dab5ebc0843b6ebdd7a080890c65a967fbc
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-11-06 14:09:08 +0530

    Scripted auto-commit on change (2022-11-06 14:09:08) by gitwatch.sh

--- Longest-common-subsequence/Distinct Subsequences/top-down.py
+++ Longest-common-subsequence/Distinct Subsequences/top-down.py
@@ -1,7 +1,13 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        return self.findDistinct(len(s)-1,len(t)-1)
-    
+        self.dp = [[0 for _ in range(len(t)+1)] for _ in range(len(s)+1)]
+        for i in range(len(s)+1):
+            self.dp[i][0]=1 
+        for i in range(1,len(s)+1):
+            for j in range(1,len(t)+1):
+                if s[i-1]==t[j-1]:
+                    self.dp[i][j]= self.dp[i-1][j-1] + self.dp[i-1][j]
+
     def findDistinct(self,i:int,j:int,s:str,t:str) -> int:
         if j<0:
             return 1
